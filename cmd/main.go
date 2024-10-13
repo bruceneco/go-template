f99ebea8562fe4e150f8ecd4f5bb7b24b2b6354e
Bruce Neco <bruce.neco@gmail.com>
2024-10-13 16:05:02 -0300

    feat: inject log and env to DI

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"go-template/config"
 	"time"
+
+	"github.com/ipfans/fxlogger"
+	"github.com/rs/zerolog/log"
 
 	"go.uber.org/fx"
 )
 
 func main() {
+	env := config.LoadEnv()
+	config.SetupLogger(env)
 	fx.New(
+		fx.Provide(func() *config.EnvConfig { return env }),
+		fx.WithLogger(fxlogger.WithZerolog(log.Logger)),
+		fx.Invoke(func(envConfig *config.EnvConfig) {
+			fmt.Println(envConfig.GoEnv)
+		}),
 		fx.Invoke(NewExample),
 	).Run()
 }
@@ -16,11 +28,14 @@ func main() {
 func NewExample(lc fx.Lifecycle) {
 	lc.Append(fx.Hook{
 		OnStart: func(_ context.Context) error {
+			log.Info().Msg("Starting example")
 			return nil
 		},
 		OnStop: func(_ context.Context) error {
+			log.Info().Msg("Stopping example")
 			gracefulDelay := 3
 			time.Sleep(time.Duration(gracefulDelay) * time.Second)
+			log.Info().Msg("Stopped example")
 			return nil
 		},
 	})
