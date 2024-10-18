a8acdc7db8fe1812964093f119b0a31c41745b94
Bruce Neco <bruce.neco@gmail.com>
2024-10-18 18:20:35 -0300

    chore: add adapters module to app

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
 	"go-template/config"
-	"time"
+	"go-template/internal/adapters"
 
 	"github.com/ipfans/fxlogger"
 	"github.com/rs/zerolog/log"
-
 	"go.uber.org/fx"
 )
 
@@ -17,25 +15,6 @@ func main() {
 	fx.New(
 		fx.Provide(func() *config.EnvConfig { return env }),
 		fx.WithLogger(fxlogger.WithZerolog(log.Logger)),
-		fx.Invoke(func(envConfig *config.EnvConfig) {
-			log.Info().Str("env_mode", envConfig.GoEnv.String()).Msgf("env loaded")
-		}),
-		fx.Invoke(NewExample),
+		adapters.Module,
 	).Run()
-}
-
-func NewExample(lc fx.Lifecycle) {
-	lc.Append(fx.Hook{
-		OnStart: func(_ context.Context) error {
-			log.Info().Msg("Starting example")
-			return nil
-		},
-		OnStop: func(_ context.Context) error {
-			log.Info().Msg("Stopping example")
-			gracefulDelay := 3
-			time.Sleep(time.Duration(gracefulDelay) * time.Second)
-			log.Info().Msg("Stopped example")
-			return nil
-		},
-	})
 }
